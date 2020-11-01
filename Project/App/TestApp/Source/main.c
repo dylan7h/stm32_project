f85780f3f15accf5fe3a4fe3926722a570a70c39
Jun-Ki, Hong <dylanhong920509@gmail.com>
2020-11-01 14:18:32 +0900

    first commit

---
+++ Project/App/TestApp/Source/main.c
@@ -0,0 +1,12 @@
+#include "Type.h"
+
+int main(void)
+{
+
+    while(true)
+    {
+        
+    }
+
+    return 0;
+}
---
+++ Project/Common/Include/Type.h
@@ -0,0 +1,31 @@
+#ifndef __TYPE__H__
+#define __TYPE__H__
+
+/* IO definitions (access restrictions to peripheral registers) */
+/**
+    \defgroup CMSIS_glob_defs CMSIS Global Defines
+
+    <strong>IO Type Qualifiers</strong> are used
+    \li to specify the access to peripheral variables.
+    \li for automatic generation of peripheral register debug information.
+*/
+#ifdef __cplusplus
+  #define   __I     volatile             /*!< Defines 'read only' permissions */
+#else
+  #define   __I     volatile const       /*!< Defines 'read only' permissions */
+#endif
+#define     __O     volatile             /*!< Defines 'write only' permissions */
+#define     __IO    volatile             /*!< Defines 'read / write' permissions */
+
+/* following defines should be used for structure members */
+#define     __IM     volatile const      /*! Defines 'read only' structure member permissions */
+#define     __OM     volatile            /*! Defines 'write only' structure member permissions */
+#define     __IOM    volatile            /*! Defines 'read / write' structure member permissions */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "stm32f303xe.h"
+
+/*@} end of group Cortex_M4 */
+
+#endif  //!__TYPE__H__
